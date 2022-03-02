469cb53190069cae392495795216471c9c3ccbbe
tanmaykhandual <83201495+tanmaykhandual@users.noreply.github.com>
2022-03-02 13:00:52 +0530

    project

---
+++ Adding_Product/src/main/java/com/simplilearn/config/HibernateUtil.java
@@ -0,0 +1,24 @@
+package com.simplilearn.config;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.simplilearn.entity.EProduct;
+
+public class HibernateUtil {
+
+	public static SessionFactory buildSessionFactory() {
+		
+		
+		Configuration cfg = new Configuration();
+		cfg.configure("hibernate.cfg.xml");
+		cfg.addAnnotatedClass(EProduct.class);
+		
+				
+		SessionFactory sf = cfg.buildSessionFactory();
+
+		return sf;
+	}
+
+}
+
